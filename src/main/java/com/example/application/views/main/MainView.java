00a82ea6221db8caa8fbe0bcdb1320d904e3b056
ilyamironow <miilig@yandex.ru>
2022-04-20 17:40:09 +0300

    1) replaced GitHub, LinkedIn with icons 2) corrected slogan formatting 3) text to russian

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -1,5 +1,6 @@
 package com.example.application.views.main;
 
+import com.vaadin.flow.component.HasText;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.dependency.CssImport;
@@ -25,9 +26,18 @@ public class MainView extends VerticalLayout {
     public MainView() {
         setSizeFull();
 
-        Anchor resumeAnchor = new Anchor("https://resume.io/r/JPx5Nn98Q", "Resume");
-        Anchor githubAnchor = new Anchor("https://github.com/ilyamironow", "GitHub");
-        Anchor linkedinAnchor = new Anchor("https://www.linkedin.com/in/ilyamironow", "LinkedIn");
+        Image githubIcon = new Image("icons/github.png", "");
+        githubIcon.setWidth(32, Unit.PIXELS);
+        Image linkedinIcon = new Image("icons/linkedin.png", "");
+        linkedinIcon.setWidth(32, Unit.PIXELS);
+
+        Anchor resumeAnchor = new Anchor("https://resume.io/r/JPx5Nn98Q",
+                "Резюме");
+        Anchor githubAnchor = new Anchor("https://github.com/ilyamironow", githubIcon);
+        Anchor linkedinAnchor = new Anchor("https://www.linkedin.com/in/ilyamironow",
+                linkedinIcon);
+        githubAnchor.setTarget("_blank");
+        linkedinAnchor.setTarget("_blank");
 
         Tab resume = new Tab(resumeAnchor);
         Tab payment = new Tab(githubAnchor);
@@ -37,20 +47,20 @@ public class MainView extends VerticalLayout {
         tabs.addThemeVariants(TabsVariant.LUMO_MINIMAL);
         tabs.addClassName("tabs");
 
-        Label nameLabel = new Label("Ilya Mironov / Software Developer");
+        Label nameLabel = new Label("Илья Миронов / Software Developer");
         nameLabel.getStyle().set("font-size", "1.4em");
         nameLabel.getStyle().set("font-weight", "400");
 
         /* big slogan styling - start */
-        Label titleLabel = new Label("приложения.\n");
+        Label titleLabel = new Label("приложения.");
         titleLabel.addClassName("animate");
 
         Paragraph paragraph = new Paragraph(
-                new Text("Я создаю "), titleLabel);
+                new Text("Я создаю\n"), titleLabel);
 
         paragraph.getStyle().set("font-size", "5em");
         paragraph.getStyle().set("font-weight", "bold");
-        paragraph.setWidth(600, Unit.PIXELS);
+        paragraph.setWhiteSpace(HasText.WhiteSpace.PRE_LINE);
         /* big slogan styling - end */
 
         Image img = new Image("images/photo_2022-04-17_00-33-50.jpg", "");
@@ -63,5 +73,19 @@ public class MainView extends VerticalLayout {
 
         setHorizontalComponentAlignment(Alignment.CENTER, tabs, horizontalLayout, nameLabel);
         add(tabs, horizontalLayout, nameLabel);
+
+
+        /* these dates should be changed later */
+        // TODO: 20.04.2022 move nameLabel to the left
+
+        // TODO: 20.04.2022 change main photo
+
+        // TODO: 20.04.2022 change slogan про баги?
+
+        // TODO: 21.04.2022 add label "Проекты" with arrow below
+
+        // TODO: 22.04.2022 add python project and make arrow and label navigate to it
+
+        // TODO: 23.04.2022 deploy
     }
 }
